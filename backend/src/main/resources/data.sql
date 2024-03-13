62bd3876fc5bf7f21551165b88933fde3a05a5d0
jeghtner <jeghtner@gmail.com>
2024-03-13 11:06:34 +0900

    fix : 불필요 파일 삭제

--- backend/src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO member (email, nickname, birth_year, gender, asset, rank_point, win, lose, single_avg_roi, multi_avg_roi)
-VALUES
-    ('john@example.com', 'John Doe', 1990, 'MAN', 10000000, 100, 50, 50, 0.75, 0.65),
-    ('jane@example.com', 'Jane Doe', 1992, 'WOMAN', 10000000, 150, 75, 25, 0.85, 0.70),
-    ('alex@example.com', 'Alex Smith', 1985, 'MAN', 10000000, 80, 40, 60, 0.60, 0.50);
