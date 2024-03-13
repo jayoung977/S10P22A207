45a6756993d4d7c7de4081861c5384311417cc36
이용수 <dldydtn0805@naver.com>
2024-03-13 15:07:46 +0900

    Merge/#22 profile page tailwind css part 1

--- frontend/app/profile/[user-id]/ProfileRecordInfoBoard.tsx
+++ frontend/app/profile/[user-id]/ProfileRecordInfoBoard.tsx
@@ -2,7 +2,7 @@ export default function UserRecordInfoBoard() {
   return (
     <div className="shadow row-span-5 relative overflow-auto max-h-96">
       <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+        <thead className="text-xs bg-small-component-3 text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
           <tr>
             <th scope="col" className="px-6 py-3">
               날짜
