29bebe5a8bdc793ee4ecd2f75100763bd54856b9
권원영 <onyo9@naver.com>
2024-03-14 10:42:16 +0900

    Merge/#31 multi game page static params part 1

--- frontend/app/multi/[game_id]/wait/page.tsx
+++ frontend/app/multi/[game_id]/wait/page.tsx
@@ -1,12 +1,11 @@
-import MultiWait from "./multigame"
+import MultiWait from "./multigame";
 
-
-export async function generateStaticParams(){
-  return [{game_id: '1'}]
+export async function generateStaticParams() {
+  // 반환 값을 객체 대신 문자열 배열로 변경
+  return [{ game_id: "1" }].map((param) => param.game_id);
 }
 
-
-export default function Page(params: {game_id: string}){
-  const { game_id } = params
-  return <MultiWait/>
+export default function Page({ params }: { params: { game_id: string } }) {
+  const { game_id } = params;
+  return <MultiWait />;
 }
