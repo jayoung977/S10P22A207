d047b3ce0e56a8302fb9dba7ec89cbce659fc265
권원영 <onyo9@naver.com>
2024-03-14 10:30:10 +0900

    Merge/#31 multi game page static params part 1

--- frontend/app/multi/[game_id]/wait/page.tsx
+++ frontend/app/multi/[game_id]/wait/page.tsx
@@ -1,45 +1,12 @@
-'use client'
-
-import ChartImage from '../chartImage' 
-import Chat from '../chat'
-import GameUser from './gameUser'
-import RoomInfo from './roomInfo'
-import FriendSearch from './friendSearch'
-import GameRule from './gameRule'
-import Header from './header'
-
-// export async function generateStaticParams(){
-//   return [{game_id: '1'}]
-// }
+import MultiWait from "./multigame"
 
 
-export default function MultiWait(){
-  // const { game_id } = params;
-  return (
-    <div className="grid grid-rows-12 h-screen border-separate">
-      <Header/>
-      <div className="row-span-11 grid grid-cols-12 border">
-        <aside className="col-span-3 grid grid-rows-12 text-center">
-          <RoomInfo/>
-          <FriendSearch/>
-          <GameRule/>
-        </aside>
-        <main className="col-span-6 grid grid-rows-12">
-          <ChartImage/>
-          <div className="border grid grid-cols-12 row-span-4">
-            <Chat/>
-            <div className='col-span-2 bg-gray-500 text-white text-center'>게임 대기 중 .....</div>
-          </div>
-        </main>
-        <aside className="col-span-3 grid grid-rows-6">
-          <GameUser/>
-          <GameUser/>
-          <GameUser/>
-          <GameUser/>
-          <GameUser/>
-          <GameUser/>
-        </aside>
-      </div>
-    </div>
-  )
+export async function generateStaticParams(){
+  return [{game_id: '1'}]
+}
+
+
+export default async function Page(params: {game_id: string}){
+  const { game_id } = params
+  return <MultiWait/>
 }
--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    distDir: 'build',
-    output: "export",
+    // distDir: 'build',
+    output: "standalone",
 };
 
 export default nextConfig;
