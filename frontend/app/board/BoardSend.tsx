54a878827eb65cba3a14f3fe9f8106832edafb04
이용수 <dldydtn0805@naver.com>
2024-03-14 14:00:05 +0900

    Merge/#33 type error fix

--- frontend/app/board/BoardSend.tsx
+++ frontend/app/board/BoardSend.tsx
@@ -62,8 +62,8 @@ export default function BoardSend() {
             </button>
             <textarea
               id="chat"
-              rows="5"
-              class="block mx-4 p-2.5 w-full text-sm text-gray-900 bg-white rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              rows={5}
+              className="block mx-4 p-2.5 w-full text-sm text-gray-900 bg-white rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               placeholder="Your message..."
             ></textarea>
             <button
