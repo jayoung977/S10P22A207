a2cb57ed9473f53091f4653e56545456ae9e2393
이용수 <dldydtn0805@naver.com>
2024-03-14 13:52:59 +0900

    Merge/#33-community-quiz-page-tailwind-css

---
+++ frontend/app/board/BoardRecieve.tsx
@@ -0,0 +1,57 @@
+export default function BoardReceive() {
+  return (
+    <div
+      className="overflow-auto"
+      style={{ maxHeight: "40vh" }}
+    >
+      <div className="col-start-2 col-end-10  rounded-md bg-small-12 m-2 w-200 h-100">
+        글 말풍선
+      </div>
+      <div className="col-start-2 col-end-10  rounded-md bg-small-12 m-2 w-200 h-100">
+        글 말풍선
+      </div>
+      <div className="col-start-2 col-end-10  rounded-md bg-small-12 m-2 w-200 h-100">
+        글 말풍선
+      </div>
+      <div className="col-start-2 col-end-10  rounded-md bg-small-12 m-2 w-200 h-100">
+        글 말풍선
+      </div>
+      <div className="col-start-2 col-end-10  rounded-md bg-small-12 m-2 w-200 h-100">
+        글 말풍선
+      </div>
+      <div className="col-start-2 col-end-10  rounded-md bg-small-12 m-2 w-200 h-100">
+        글 말풍선
+      </div>
+      <div className="col-start-2 col-end-10  rounded-md bg-small-12 m-2 w-200 h-100">
+        글 말풍선
+      </div>
+      <div className="col-start-2 col-end-10  rounded-md bg-small-12 m-2 w-200 h-100">
+        글 말풍선
+      </div>
+      <div className="col-start-2 col-end-10  rounded-md bg-small-12 m-2 w-200 h-100">
+        글 말풍선
+      </div>
+      <div className="col-start-2 col-end-10  rounded-md bg-small-12 m-2 w-200 h-100">
+        글 말풍선
+      </div>
+      <div className="col-start-2 col-end-10  rounded-md bg-small-12 m-2 w-200 h-100">
+        글 말풍선
+      </div>
+      <div className="col-start-2 col-end-10  rounded-md bg-small-12 m-2 w-200 h-100">
+        글 말풍선
+      </div>
+      <div className="col-start-2 col-end-10  rounded-md bg-small-12 m-2 w-200 h-100">
+        글 말풍선
+      </div>
+      <div className="col-start-2 col-end-10  rounded-md bg-small-12 m-2 w-200 h-100">
+        글 말풍선
+      </div>
+      <div className="col-start-2 col-end-10  rounded-md bg-small-12 m-2 w-200 h-100">
+        글 말풍선
+      </div>
+      <div className="col-start-2 col-end-10  rounded-md bg-small-12 m-2 w-200 h-100">
+        글 말풍선
+      </div>
+    </div>
+  );
+}
