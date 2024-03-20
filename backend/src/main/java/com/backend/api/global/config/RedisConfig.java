769529266f04574936160d633394fb315b51d05d
ChoiChangho <chh9450@naver.com>
2024-03-20 15:14:11 +0900

    feat : #30 Single Game security 적용

--- backend/src/main/java/com/backend/api/global/config/RedisConfig.java
+++ backend/src/main/java/com/backend/api/global/config/RedisConfig.java
@@ -1,22 +1,14 @@
 package com.backend.api.global.config;
 
-import com.backend.api.domain.notice.service.RedisSubService;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.MessageListener;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
-import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Log4j2
@@ -61,23 +53,6 @@ public class RedisConfig {
         return container;
     }
 
-    @Bean
-    MessageListener messageListenerAdapter() {
-        return new MessageListenerAdapter(new RedisSubService());
-    }
-
-    @Bean
-    ChannelTopic topic(){
-        return new ChannelTopic("topic1");
-    }
-
-    @Bean
-    public ObjectMapper objectMapper() {
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
-        mapper.registerModules(new JavaTimeModule(), new Jdk8Module());
-        return mapper;
-    }
 }
 
 
