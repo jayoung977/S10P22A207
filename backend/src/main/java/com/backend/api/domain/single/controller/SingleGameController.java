4d257b9f13cd231c7a444d105823da194334a6da
ChoiChangho <chh9450@naver.com>
2024-03-20 15:07:16 +0900

    feat : #30 Single Game security 적용

--- backend/src/main/java/com/backend/api/domain/single/controller/SingleGameController.java
+++ backend/src/main/java/com/backend/api/domain/single/controller/SingleGameController.java
@@ -12,6 +12,7 @@ import com.backend.api.global.security.userdetails.CustomUserDetails;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,6 +21,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@PreAuthorize("hasAnyRole('USER')")
 @RequestMapping("/api/single")
 @RequiredArgsConstructor
 public class SingleGameController {
