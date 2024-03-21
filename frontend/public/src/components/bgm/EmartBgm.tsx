f89c5637a8a41923b10ea5db64c90be224cb00d3
이용수 <dldydtn0805@naver.com>
2024-03-22 08:47:02 +0900

    Merge/#58 quiz api link

--- frontend/public/src/components/bgm/EmartBgm.tsx
+++ frontend/public/src/components/bgm/EmartBgm.tsx
@@ -2,14 +2,13 @@
 import React, { useRef, useEffect } from "react";
 import bgm from "../../../../static/sounds/emart.mp3";
 
-
-const EmartBgm: React.FC = () => {
+export default function EmartBgm() {
   const audioRef = useRef<HTMLAudioElement>(null);
 
   useEffect(() => {
     if (audioRef.current) {
       console.log("지금 재생중");
-      audioRef.current.volume = 0.2; // 볼륨 조절 (0.0 ~ 1.0)
+      audioRef.current.volume = 0.1; // 볼륨 조절 (0.0 ~ 1.0)
       audioRef.current.loop = true; // 반복 재생 여부
     }
   }, []);
@@ -19,6 +18,4 @@ const EmartBgm: React.FC = () => {
       <source src={bgm} type="audio/mpeg" />
     </audio>
   );
-};
-
-export default EmartBgm;
+}
