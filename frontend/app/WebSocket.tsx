d4c7aefdfb6916399c1ccbbd6b89cb1fd09eb1fa
dldydtn0805 <ssafysoo95@gmail.com>
2024-03-30 00:49:55 +0900

    feat/#91-multigame-websocket-sse

--- frontend/app/WebSocket.tsx
+++ frontend/app/WebSocket.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { useWebSocket } from "@/public/src/hooks/useWebSocket";
+import useFetchUserInfo from "@/public/src/hooks/useFetchUserInfo";
 export default function WebSocket() {
   useWebSocket();
   return <></>;
