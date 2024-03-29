ef7e277e9f6ad8c480110bbd55531004d130d47a
이용수 <dldydtn0805@naver.com>
2024-03-30 00:56:36 +0900

    Merge/#91 multigame websocket sse

--- frontend/app/multi/room/[room_id]/friendSearch.tsx
+++ frontend/app/multi/room/[room_id]/friendSearch.tsx
@@ -1,90 +1,117 @@
-import SearchBar from '../../searchBar'
-import Image from 'next/image'
-import ProfileImage from '@/public/src/assets/images/profile-image.png'
-import Swal from 'sweetalert2'
-import multigameStore from '@/public/src/stores/multi/MultiGameStore'
-import { useEffect, useState } from 'react'
-import { useQuery, UseQueryResult } from 'react-query'
-import { Friend, FriendInfo } from '@/public/src/stores/user/userStore'
+import SearchBar from "../../searchBar";
+import Image from "next/image";
+import ProfileImage from "@/public/src/assets/images/profile-image.png";
+import Swal from "sweetalert2";
+import multigameStore from "@/public/src/stores/multi/MultiGameStore";
+import { useEffect, useState } from "react";
+import { useQuery, UseQueryResult } from "react-query";
+import { Friend, FriendInfo } from "@/public/src/stores/user/userStore";
+import axios from "axios";
+import { useParams } from "next/navigation";
 
-const fetchFriendInfo = async() => {
-  const token = sessionStorage.getItem('accessToken')
-  const response = await fetch('https://j10a207.p.ssafy.io/api/friend/list',{
+const fetchFriendInfo = async () => {
+  const token = sessionStorage.getItem("accessToken");
+  const response = await fetch("https://j10a207.p.ssafy.io/api/friend/list", {
     headers: {
-      Authorization: `Bearer ${token}`
-    }
-  })
-  return response.json()
-}
-
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  return response.json();
+};
 
 export default function FriendSearch() {
-  const {data, isLoading, error}: UseQueryResult<FriendInfo,Error> = useQuery('FriendInfo', fetchFriendInfo) 
-  
-  
-  // 친구목록 react-query로 구현
-  const { result }: { result: Friend[] } = data
-  ? data
-  : { result: [] };
-  
+  const { data, isLoading, error }: UseQueryResult<FriendInfo, Error> =
+    useQuery("FriendInfo", fetchFriendInfo);
 
-  
+  // 친구목록 react-query로 구현
+  const { result }: { result: Friend[] } = data ? data : { result: [] };
+
   const { searchFriend } = multigameStore();
-  const [filteredFriendList, setfilteredFriendList] = useState<Friend[]>([])
-  
+  const [filteredFriendList, setfilteredFriendList] = useState<Friend[]>([]);
+
   useEffect(() => {
     const filtered: Friend[] = result.filter((friend) =>
-    friend.nickname.includes(searchFriend)
+      friend.nickname.includes(searchFriend)
     );
     setfilteredFriendList(filtered);
   }, [searchFriend, result]);
-  
-  const inviteFriend = (nickname: string, memberId: number) => {
-    console.log(`${nickname} 초대!`)
+
+  const invitationRequest = async (request: any) => {
+    const response = await axios({
+      method: "post",
+      url: "https://j10a207.p.ssafy.io/api/multi/invite",
+      headers: {
+        Authorization: `Bearer ${sessionStorage.getItem("accessToken")}`,
+      },
+      data: request,
+    });
+    console.log(response.data);
+    return response.data;
+  };
+  const params = useParams<{ room_id?: string }>();
+  const room_id: string | undefined = params.room_id;
+
+  const inviteFriend = (receiver_id: number) => {
+    const data = {
+      roomId: room_id,
+      receiver: receiver_id,
+    };
+    console.log();
+    invitationRequest(data);
     Swal.fire({
-      text: `${nickname}에게 초대를 발송했습니다.`,
-      icon: 'success'
-    })
-  }
-  
-  
+      text: `${receiver_id}에게 초대를 발송했습니다.`,
+      icon: "success",
+    });
+  };
+
   if (isLoading) {
     return <div className="rainbow"></div>;
   }
-  
+
   if (error) {
     return <div>Error: {error.message}</div>;
   }
-
+  console.log(filteredFriendList);
   return (
     <div className="row-span-3 border-e grid grid-rows-6">
-      <div className='row-span-1 flex justify-center border-b gap-2 items-center'>
+      <div className="row-span-1 flex justify-center border-b gap-2 items-center">
         <div>친구초대</div>
-        <SearchBar/>
+        <SearchBar />
       </div>
-      <div className='overflow-auto row-span-5' style={{height: 'calc(35vh)'}}>
-        {
-          filteredFriendList.map((friend: Friend, i: number)=> {
-            return(
-              <div key={i}  className="grid grid-cols-12 items-center bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+      <div
+        className="overflow-auto row-span-5"
+        style={{ height: "calc(35vh)" }}
+      >
+        {filteredFriendList.map((friend: Friend, i: number) => {
+          return (
+            <div
+              key={i}
+              className="grid grid-cols-12 items-center bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
+            >
               <div className="col-span-2 items-center text-gray-900 whitespace-nowrap dark:text-white">
                 <Image
                   src={ProfileImage}
-                  alt='프로필'
-                  className='rounded-full'
-                  />
+                  alt="프로필"
+                  className="rounded-full"
+                />
               </div>
-              <div className="col-span-6 text-base font-semibold">{friend.nickname}</div>
+              <div className="col-span-6 text-base font-semibold">
+                {friend.nickname}
+              </div>
               <div className="col-span-4 px-6 py-4">
-                <button onClick={()=>{
-                  inviteFriend(friend.nickname, friend.memberId)
-                  }} className='bg-blue-500 text-white px-2 py-1 rounded-md '>초대</button>
+                <button
+                  onClick={() => {
+                    inviteFriend(friend.memberId);
+                  }}
+                  className="bg-blue-500 text-white px-2 py-1 rounded-md "
+                >
+                  초대
+                </button>
               </div>
-            </div>  
-            )
-          })
-        }
+            </div>
+          );
+        })}
       </div>
     </div>
-  )
+  );
 }
--- frontend/public/src/hooks/useWebSocket.tsx
+++ frontend/public/src/hooks/useWebSocket.tsx
@@ -2,27 +2,42 @@
 import { useEffect, useRef } from "react";
 import { CompatClient, Stomp } from "@stomp/stompjs";
 import SockJS from "sockjs-client";
-import { useParams } from "next/navigation";
-import multigameStore from "../stores/multi/MultiGameStore";
 import Swal from "sweetalert2";
+import userStore from "../stores/user/userStore";
 import socketStore from "../stores/websocket/socketStore";
 
 export const useWebSocket = () => {
   const client = useRef<CompatClient>({} as CompatClient);
-  const { setClientObject, clientObject } = socketStore();
+  const { setClientObject, clientObject, setReceiveMessage, receiveMessage } =
+    socketStore();
+  const { memberId } = userStore();
 
   useEffect(() => {
-    client.current = Stomp.over(() => {
-      const sock = new SockJS("https://j10a207.p.ssafy.io/ws");
-      return sock;
-    });
-    Swal.fire("웹소켓 연결 됨");
-    setClientObject(client);
-    return () => {
-      if (client.current) {
-        client.current.disconnect();
-        Swal.fire("웹소켓 연결 안됨");
-      }
-    };
-  }, []);
+    if (memberId) {
+      client.current = Stomp.over(() => {
+        const sock = new SockJS("https://j10a207.p.ssafy.io/ws");
+        return sock;
+      });
+      Swal.fire("웹소켓 연결 됨");
+      setClientObject(client);
+
+      client.current.connect({}, () => {
+        console.log("소켓 연결했으니까 알고있어라");
+        client.current.subscribe(`/api/sub/${memberId}`, (message: any) => {
+          const parsedMessage = JSON.parse(message.body);
+          setReceiveMessage((prevReceiveMessage: any) => {
+            const copy = [...prevReceiveMessage, parsedMessage];
+            console.log(copy); // 업데이트된 값을 확인할 수 있습니다.
+            return copy;
+          });
+        });
+      });
+      return () => {
+        if (client.current) {
+          client.current.disconnect();
+          Swal.fire("웹소켓 연결 안됨");
+        }
+      };
+    }
+  }, [memberId]);
 };
