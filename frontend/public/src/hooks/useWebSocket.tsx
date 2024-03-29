5e60433cf124ce922fc8673637753d165fbec9a6
dldydtn0805 <ssafysoo95@gmail.com>
2024-03-30 02:30:07 +0900

    feat/#91-multigame-websocket-sse

--- frontend/public/src/hooks/useWebSocket.tsx
+++ frontend/public/src/hooks/useWebSocket.tsx
@@ -1,5 +1,5 @@
 // useWebSocket.ts
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { CompatClient, Stomp } from "@stomp/stompjs";
 import SockJS from "sockjs-client";
 import Swal from "sweetalert2";
@@ -8,10 +8,10 @@ import socketStore from "../stores/websocket/socketStore";
 
 export const useWebSocket = () => {
   const client = useRef<CompatClient>({} as CompatClient);
-  const { setClientObject, clientObject, setReceiveMessage, receiveMessage } =
-    socketStore();
+  const { setClientObject, clientObject } = socketStore();
   const { memberId } = userStore();
-
+  const [receiveMessage, setReceiveMessage] = useState<any>([]);
+  const { receiveMessage2, setReceiveMessage2 } = socketStore();
   useEffect(() => {
     if (memberId) {
       client.current = Stomp.over(() => {
@@ -25,11 +25,13 @@ export const useWebSocket = () => {
         console.log("소켓 연결했으니까 알고있어라");
         client.current.subscribe(`/api/sub/${memberId}`, (message: any) => {
           const parsedMessage = JSON.parse(message.body);
-          setReceiveMessage((prevReceiveMessage: any) => {
-            const copy = [...prevReceiveMessage, parsedMessage];
-            console.log(copy); // 업데이트된 값을 확인할 수 있습니다.
-            return copy;
-          });
+          if (parsedMessage.type === "MESSAGE") {
+            setReceiveMessage((prevReceiveMessage: any) => {
+              const copy = [...prevReceiveMessage, parsedMessage];
+              setReceiveMessage2(copy);
+              return copy;
+            });
+          }
         });
       });
       return () => {
