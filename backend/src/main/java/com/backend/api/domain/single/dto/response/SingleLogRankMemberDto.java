30bec4b6721f0f0b5e3db5054851617329ce8754
choijayoung <wjsrnr20@gmail.com>
2024-03-29 13:57:15 +0900

    Merge/#89 single log api

---
+++ backend/src/main/java/com/backend/api/domain/single/dto/response/SingleLogRankMemberDto.java
@@ -0,0 +1,13 @@
+package com.backend.api.domain.single.dto.response;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+public record SingleLogRankMemberDto(
+        @Schema(description = "멤버 id")
+        Long memberId,
+        @Schema(description = "닉네임")
+        String nickname,
+        @Schema(description = "수익률")
+        Double roi
+) {
+}
