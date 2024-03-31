62b0a318749e436910d3348f915e1c3b4b09f710
dldydtn0805 <ssafysoo95@gmail.com>
2024-03-31 18:10:56 +0900

    feat/#91-multigame-websocket-sse

--- frontend/app/profile/[userId]/single/[game-id]/ReviewSingleRanking.tsx
+++ frontend/app/profile/[userId]/single/[game-id]/ReviewSingleRanking.tsx
@@ -1,6 +1,6 @@
-'use client'
-import { useState } from 'react';
-import RankingUserChartModal from './RankingUserChartModal';
+"use client";
+import { useState } from "react";
+import RankingUserChartModal from "./RankingUserChartModal";
 import SingleReviewStore from "@/public/src/stores/profile/SingleReviewStore";
 import axios from "axios";
 
@@ -9,33 +9,33 @@ export default function SingleRanking() {
   const [isOpenReviewModal, setIsOpenReviewModal] = useState<boolean>(false);
   const [rankingUserData, setRankingUserData] = useState<any>([]);
 
-  function handleOpenModal () {
+  function handleOpenModal() {
     setIsOpenReviewModal(true);
   }
 
-  const fetchRankingUserRecord = async (item :any) => {
+  const fetchRankingUserRecord = async (item: any) => {
     try {
       const response = await axios({
         method: "get",
-        url : `https://j10a207.p.ssafy.io/api/single/log/member?singelGameStockId=${item.singleGameStockId}&memberId=${item.memberId}`,
+        url: `https://j10a207.p.ssafy.io/api/single/log/member?singelGameStockId=${item.singleGameStockId}&memberId=${item.memberId}`,
         headers: {
           Authorization: `Bearer ${sessionStorage.getItem("accessToken")}`,
         },
-      })
+      });
 
-      
       const res = {
-        memberId : item.memberId,
-        nickname : item.nickname,
-        stockChartList : response.data.result.stockChartList,
-        tradeList : response.data.result.tradeList
-      }
+        memberId: item.memberId,
+        nickname: item.nickname,
+        stockChartList: response.data.result.stockChartList,
+        tradeList: response.data.result.tradeList,
+      };
       setRankingUserData(res);
       handleOpenModal();
     } catch (error) {
       console.log(error);
     }
-  }
+  };
+
   return (
     <>
       <div className="row-span-5 grid grid-rows-5 bg-slate-500 rounded-md m-1">
@@ -51,29 +51,41 @@ export default function SingleRanking() {
             </tr>
           </thead>
           <tbody className="row-span-3 grid grid-rows-3 items-center">
-            {
-              rankMemberList[selectedIndex].rankMemberDtoList && rankMemberList[selectedIndex].rankMemberDtoList.length > 0 ? (
-                rankMemberList[selectedIndex]?.rankMemberDtoList.map((item :any, index :number) => (
-                  <tr 
-                    key={index} 
+            {rankMemberList[selectedIndex]?.rankMemberDtoList &&
+            rankMemberList[selectedIndex]?.rankMemberDtoList.length > 0 ? (
+              rankMemberList[selectedIndex]?.rankMemberDtoList.map(
+                (item: any, index: number) => (
+                  <tr
+                    key={index}
                     className="row-span-1 grid grid-cols-6 text-center bg-white rounded-lg m-1 hover:bg-slate-300"
-                    onClick={() => {fetchRankingUserRecord(item)}}
-                    style={{ cursor : "pointer" }}
+                    onClick={() => {
+                      fetchRankingUserRecord(item);
+                    }}
+                    style={{ cursor: "pointer" }}
                   >
-                    <td className="col-span-1">{index+1}</td>
+                    <td className="col-span-1">{index + 1}</td>
                     <td className="col-span-3">{item.nickname}</td>
-                    <td className="col-span-2">{parseFloat(item.roi.toFixed(2))}</td>                      
+                    <td className="col-span-2">
+                      {parseFloat(item.roi.toFixed(2))}
+                    </td>
                   </tr>
-                  ))
-              ) : (
-                <div className="flex items-center justify-center mt-5">등록된 랭커가 없습니다.</div>
+                )
               )
-            }
+            ) : (
+              <div className="flex items-center justify-center mt-5">
+                등록된 랭커가 없습니다.
+              </div>
+            )}
           </tbody>
         </table>
       </div>
-      <RankingUserChartModal isOpen={isOpenReviewModal} onClose={() => {setIsOpenReviewModal(false)}} data={rankingUserData}/>
+      <RankingUserChartModal
+        isOpen={isOpenReviewModal}
+        onClose={() => {
+          setIsOpenReviewModal(false);
+        }}
+        data={rankingUserData}
+      />
     </>
   );
 }
-
