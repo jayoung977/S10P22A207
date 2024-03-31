b4bc8a2388226ddfd67098bd921115d47f1bbd0e
이용수 <dldydtn0805@naver.com>
2024-03-31 15:44:19 +0900

    Merge/#91 multigame websocket sse

--- frontend/app/multi/gameroomSetting.tsx
+++ frontend/app/multi/gameroomSetting.tsx
@@ -7,26 +7,31 @@ import Pagination from "./pagination";
 import { useQuery, UseQueryResult } from "react-query";
 import multigameStore, {
   MultiRoomInfo,
-  MultiRoom,
+  MultiGameRoomInfoList,
+  ResultType,
 } from "@/public/src/stores/multi/MultiGameStore";
-
 
 const fetchMultiRoomInfo = async (pageNumber: number) => {
   const token = sessionStorage.getItem("accessToken");
-  const response = await fetch(`https://j10a207.p.ssafy.io/api/multi?pageNumber=${pageNumber}`, {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
+  const response = await fetch(
+    `https://j10a207.p.ssafy.io/api/multi?pageNumber=${pageNumber}`,
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
   return response.json();
 };
 
 export default function GameRoomSetting() {
-  const [rooms, setRooms] = useState<MultiRoom[]>([]);
+  const [rooms, setRooms] = useState<MultiGameRoomInfoList[]>([]);
   const { pageNumber } = multigameStore();
   const { data, isLoading, error }: UseQueryResult<MultiRoomInfo, Error> =
-  // pageNumber를 의존성 변수로 추가하면 pageNumber에 따라 react-query 실행
-    useQuery(['MultiRoomInfo', pageNumber], () => fetchMultiRoomInfo(pageNumber));
+    // pageNumber를 의존성 변수로 추가하면 pageNumber에 따라 react-query 실행
+    useQuery(["MultiRoomInfo", pageNumber], () =>
+      fetchMultiRoomInfo(pageNumber)
+    );
   type ColorClass = { [key: number]: string };
   const RoomColor: ColorClass = {
     0: "bg-small-1",
@@ -52,15 +57,6 @@ export default function GameRoomSetting() {
   const handleQuickstart = () => {
     router.push("multi/room/1");
   };
-  
-  useEffect(()=> {
-    if(data?.result){
-      setRooms(data?.result);
-    } else {
-      setRooms([])
-    }
-  },[data])
-
 
   if (isLoading) {
     return <div className="rainbow"></div>;
@@ -70,11 +66,9 @@ export default function GameRoomSetting() {
     return <div>Error: {error.message}</div>;
   }
 
-  const { result }: { result: MultiRoom[] | null } = data
+  const { result }: { result: ResultType | null } = data
     ? data
     : { result: null };
-  console.log(result)
-
 
   return (
     <div className="col-span-8 grid grid-rows-12 p-2">
@@ -188,10 +182,12 @@ export default function GameRoomSetting() {
       </div>
       {/* 게임방 목록 */}
       <div className="bg-background-1 row-span-8 rounded-md grid grid-cols-12 grid-rows-3 shadow-md gap-1">
-        {rooms.map((room: MultiRoom, i: number) => (
-
+        {result?.multiGameRoomInfoList.map((room: MultiGameRoomInfoList, i: number) => (
           <div className="col-span-6 row-span-1 p-1 m-1 rounded-md" key={i}>
-            <GameRoom color={RoomColor[(pageNumber - 1 + i) % 13]} room={room} />
+            <GameRoom
+              color={RoomColor[(pageNumber - 1 + i) % 13]}
+              room={room}
+            />
           </div>
         ))}
       </div>
