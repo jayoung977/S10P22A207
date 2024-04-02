d27331250ecbb3e9cd82bc54cfa83fca4b182ac4
김민규 <minkyu1180@naver.com>
2024-04-02 14:38:58 +0900

    feat : #115 멀티 게임 차트 컴포넌트 구현

--- frontend/public/src/stores/multi/MultiGameStore.tsx
+++ frontend/public/src/stores/multi/MultiGameStore.tsx
@@ -17,6 +17,17 @@ type Store = {
   setPageNumber: (value: number) => void;
   isWaiting: boolean;
   setIsWaiting: (value: boolean) => void;
+
+  stockId :number,
+  setStockId :(value :number) => void;
+  stockChartList :stockChartInterface[] | any;
+  setStockChartList :(value :stockChartInterface[]) => void;
+  roundNumber :number;
+  setRoundNumber :(value :number) => void;
+  maxRoundNumber :number;
+  setMaxRoundNumber :(value :number) => void;
+  turn :number;
+  setTurn :(value :number) => void;
 };
 
 export interface MultiGameRoomInfoList {
@@ -38,6 +49,28 @@ export interface MultiRoomInfo {
   result: ResultType;
 }
 
+// 종목의 각 날짜별 금액 데이터(OHLC, Volume)
+export interface stockChartInterface {
+  date :string;
+  markerPrice :number;
+  highPrice :number;
+  lowPrice :number;
+  endPrice :number;
+  tradingVolume :number;
+}
+
+// stockId값을 종목 id로 가지는 종목의 350일 간의 차트 데이터
+export interface stockChartDataInterface {
+  stockId :number,
+  stockChartList :stockChartInterface[];
+}
+
+// 게임 페이지 렌더링 시 api 요청으로 받는 response.data.result
+export interface stockInfoDataInterface {
+  gameId :number;
+  stockChartData :stockChartDataInterface;
+}
+
 const multigameStore = create<Store>((set: any) => ({
   toggleTab: "all",
   setToggleTab: (value) => set({ toggleTab: value }),
@@ -55,6 +88,18 @@ const multigameStore = create<Store>((set: any) => ({
   setPageNumber: (value) => set({ pageNumber: value }),
   isWaiting: false,
   setIsWaiting: (value) => set({ isWaiting: value }),
+  // multi game state value
+  stockId :0,
+  setStockId: (value) => set({ stockId: value }),
+  stockChartList: [],
+  setStockChartList: (value) => set({ stockChartList: value }),
+  roundNumber: 0,
+  setRoundNumber :(value) => set({ roundNumber: value }),
+  maxRoundNumber :0,
+  setMaxRoundNumber: (value) => set({ maxRoundNumber: value }),
+  turn: 1,
+  setTurn: (value) => set({ turn: value }),
+  
 }));
 
 export default multigameStore;
