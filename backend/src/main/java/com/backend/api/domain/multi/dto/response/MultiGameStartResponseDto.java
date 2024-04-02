e1a22914c5b93a0abdf7ccc0923de1666c634151
최창호 <chh9450@naver.com>
2024-04-02 11:24:36 +0900

    Merge/#114 - 멀티게임 구현

--- backend/src/main/java/com/backend/api/domain/multi/dto/response/MultiGameStartResponseDto.java
+++ backend/src/main/java/com/backend/api/domain/multi/dto/response/MultiGameStartResponseDto.java
@@ -1,15 +1,11 @@
 package com.backend.api.domain.multi.dto.response;
 
-import com.backend.api.domain.single.dto.response.StockChartDataDto;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 public record MultiGameStartResponseDto(
 
     @Schema(description = "레디스에 저장된 게임 Id")
-    Long gameId,
-
-    @Schema(description = "종목별 차트 response Dto")
-    StockChartDataDto stockChartData
+    Long gameId
 
 ) {
 
