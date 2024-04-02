e83999e5eaac9f450ac87cffb9b89b6bec39cf97
jeghtner <jeghtner@gmail.com>
2024-04-03 08:11:58 +0900

    feat : #106 멀티게임방 플레이중 상태 추가, 플레이 중일 시 리스트에서 제외, 입장방지

--- backend/src/main/java/com/backend/api/domain/multi/service/MultiGameService.java
+++ backend/src/main/java/com/backend/api/domain/multi/service/MultiGameService.java
@@ -212,17 +212,17 @@ public class MultiGameService {
 
 	public MultiGameStartResponseDto startMultiGame(Long memberId, MultiGameStartRequestDto dto) {
 		log.info("MULTIGAMESTART:::");
-		MultiWaitingRoom multiwaitingRoom = getWaitingRoom(dto.roomId());
+		MultiWaitingRoom multiWaitingRoom = getWaitingRoom(dto.roomId());
 		/* 예외 처리*/
 		// if (multiwaitingRoom.getParticipantIds().size() < 2) {
 		// 	throw new BaseExceptionHandler(ErrorCode.NOT_ENOUGH_PARTICIPANTS);
 		// }
-		if(multiwaitingRoom.getIsPlaying()) {
+		if(multiWaitingRoom.getIsPlaying()) {
 			throw new BaseExceptionHandler(ErrorCode.IS_PLAYING);
 		}
-		if(!multiwaitingRoom.getIsPlaying()){
-			multiwaitingRoom.setIsPlaying(true);
-			redisTemplate.opsForValue().set("multiGame:" + dto.roomId(), multiwaitingRoom);
+		if(!multiWaitingRoom.getIsPlaying()){
+			multiWaitingRoom.setIsPlaying(true);
+			redisTemplate.opsForValue().set("multiGame:" + dto.roomId(), multiWaitingRoom);
 		}
         Long gameId = null;
         if (dto.roundNumber() == 1) {
