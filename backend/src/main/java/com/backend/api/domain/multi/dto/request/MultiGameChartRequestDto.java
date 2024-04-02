7172ec110f9099f9a94f96ec4238bf948b45dd0e
최창호 <chh9450@naver.com>
2024-04-02 13:20:10 +0900

    Merge/#105-멀티 게임 Response 변경

--- backend/src/main/java/com/backend/api/domain/multi/dto/request/MultiGameChartRequestDto.java
+++ backend/src/main/java/com/backend/api/domain/multi/dto/request/MultiGameChartRequestDto.java
@@ -3,18 +3,23 @@ package com.backend.api.domain.multi.dto.request;
 import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.List;
 
-public record MultiGameStartRequestDto(
+public record MultiGameChartRequestDto(
+
     @Schema(description = "게임 참가자 Id")
     List<Long> playerIds,
 
     @Schema(description = "라운드")
     Integer roundNumber,
 
+    @Schema(description = "총 라운드 수")
+    Integer maxRoundNumber,
+
+    @Schema(description = "게임 ID")
+    Long gameId,
+
     @Schema(description = "방 ID")
     Long roomId
 
-)
-
-{
+) {
 
 }
