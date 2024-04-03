0626028b9f8e56d003b6d7ae8cbb1d370ea2d5c6
김민규 <minkyu1180@naver.com>
2024-04-03 15:26:37 +0900

    fix : #115 멀티게임 버그 수정

--- frontend/app/multi/room/[room_id]/play/[game_id]/roundChart.tsx
+++ frontend/app/multi/room/[room_id]/play/[game_id]/roundChart.tsx
@@ -492,7 +492,7 @@ export default function RoundChart({ data }: any) {
             (window as any).handleShowPlot = null;
         };
     
-    }, [data]);
+    }, [day]);
 
     const playClickSound = useClickSound();
   
