9d6a45e64a69d3174541c9f9106acaa3695a9bd5
이용수 <dldydtn0805@naver.com>
2024-04-04 08:58:39 +0900

    Merge/#91 multigame websocket sse

--- frontend/app/multi/room/[room_id]/play/[game_id]/finalUser.tsx
+++ frontend/app/multi/room/[room_id]/play/[game_id]/finalUser.tsx
@@ -1,33 +1,39 @@
-'use client'
-import ProfileImage from '@/public/src/assets/images/profile-image.png'
-import useClickSound from '@/public/src/components/clickSound/DefaultClick'
-import Image from 'next/image'
-
-export default function FinalUser(){
+"use client";
+import ProfileImage from "@/public/src/assets/images/profile-image.png";
+import useClickSound from "@/public/src/components/clickSound/DefaultClick";
+import socketStore from "@/public/src/stores/websocket/socketStore";
+import Image from "next/image";
+import useGetProfileImage from "@/public/src/hooks/useGetProfileImage";
+export default function FinalUser({ player }: any) {
   const playClickSound = useClickSound();
 
-  return(
+  return (
     <div
       onClick={playClickSound}
-      className="row-span-1 grid grid-cols-12 border rounded-md p-2 m-1 items-center bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-      <div className='col-span-1'>
-        <div className='border rounded-md px-2 py-1 ms-2 text-center bg-gray-300'>rank</div>
-      </div>
-      <div className=' col-start-3 col-span-2'>
-        <Image
-          src={ProfileImage}
-          alt='profile-image'
-          width={60}
-          height={60}
-          style={{borderRadius: '50%'}}
+      className="row-span-1  border rounded-md p-2 m-1 items-center bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
+    >
+      <div className="grid grid-cols-12 ">
+        <div className="col-span-1">
+          <div className="border rounded-md px-2 py-1 ms-2 text-center bg-gray-300">
+            {player.rank}
+          </div>
+        </div>
+        <div className=" col-start-3 col-span-2">
+          <Image
+            src={useGetProfileImage(player.rank * 10000000)}
+            alt="profile-image"
+            width={60}
+            height={60}
+            style={{ borderRadius: "50%" }}
           />
-      </div>
-      <div className='col-span-3'>
-        <div className='text-lg'>nickname</div>
-      </div>
-      <div className="col-span-5">
-        <div>totalAsset (roi)</div>
+        </div>
+        <div className="col-span-3">
+          <div className="text-lg">{player.nickName}</div>
+        </div>
+        <div className="col-span-5">
+          <div>{player.totalAsset.toLocaleString()}원</div>
+        </div>
       </div>
     </div>
-  )
+  );
 }
