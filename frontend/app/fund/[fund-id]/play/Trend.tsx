b407e2fb909143a36580644d80bb3ceaf0c0dbc3
권원영 <onyo9@naver.com>
2024-04-03 10:54:59 +0900

    Merge/#87 multi game sockjs part 2

--- frontend/app/fund/[fund-id]/play/Trend.tsx
+++ frontend/app/fund/[fund-id]/play/Trend.tsx
@@ -1,7 +1,7 @@
 export default function Trend ({ rank, data } :any) {
 
     return (
-        <div className="grid grid-cols-6 bg-small-9 rounded-xl m-1">
+        <div className="grid grid-cols-6 bg-small-1 rounded-xl m-1">
             <div className="col-span-1 text-center text-textColor-2">{rank}.</div>
             <div className="col-span-5 text-start text-textColor-2">{data.트렌드}</div>
         </div>
