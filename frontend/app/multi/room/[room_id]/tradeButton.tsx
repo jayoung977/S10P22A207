39c34e9946bb735e570315cec8759eabf0c84f19
권원영 <onyo9@naver.com>
2024-04-03 21:20:54 +0900

    Merge/#87 multi game sockjs part 2

--- frontend/app/multi/room/[room_id]/tradeButton.tsx
+++ frontend/app/multi/room/[room_id]/tradeButton.tsx
@@ -2,29 +2,55 @@
 import { useState, useEffect } from "react";
 import TradeModal from "./play/[game_id]/TradeModal";
 import useClickSound from "@/public/src/components/clickSound/DefaultClick";
-import axios from "axios";
+import multigameStore from "@/public/src/stores/multi/MultiGameStore";
 import socketStore from "@/public/src/stores/websocket/socketStore";
+
+
 export default function TradeButtons() {
   // 거래 모달창 open 여부
   const [isOpenTradeModal, setIsOpenTradeModal] = useState<boolean>(false);
   // 매수 / 매도 / 공매도
-  const [tradeType, setTradeType] = useState<string>("");
+  const { tradeType,
+          setTradeType,
+          tradeStocksAmount,
+          setTradeStocksAmount,
+          isAvailableTradeStocks,
+          setIsAvailableTradeStocks
+        } = multigameStore();
   const playClickSound = useClickSound();
+  const { cash, stockAmount, shortStockAmount, todayEndPrice } = socketStore();
+  const fee = tradeType === 'short-selling' ? 1.0025 : tradeType === 'buy' ? 1.0015 : 1;
+  const sellTradeStocks = Number(stockAmount)
+  const buyTradeStocks = Math.floor(cash / (todayEndPrice * fee))
+  const closeShort = shortStockAmount
+
   const handleTradeTurn = (e: KeyboardEvent) => {
     if (e.key === "q") {
       playClickSound();
+      setTradeStocksAmount(0);
       setTradeType("buy");
+      setIsAvailableTradeStocks(buyTradeStocks)
       setIsOpenTradeModal(true);
     } else if (e.key === "w") {
       playClickSound();
+      setTradeStocksAmount(0);
       setTradeType("sell");
+      setIsAvailableTradeStocks(sellTradeStocks)
       setIsOpenTradeModal(true);
     } else if (e.key === "e") {
       playClickSound();
-      setTradeType("shortSell");
+      setTradeStocksAmount(0);
+      setTradeType('short-selling');
+      setIsAvailableTradeStocks(buyTradeStocks)
+      setIsOpenTradeModal(true);
+    } else if (e.key === "c") {
+      playClickSound();
+      setTradeStocksAmount(0);
+      setIsAvailableTradeStocks(closeShort)
+      setTradeType("close-short");
       setIsOpenTradeModal(true);
     }
-  };
+  }
 
 
   useEffect(() => {
@@ -50,7 +76,9 @@ export default function TradeButtons() {
         <button
           onClick={() => {
             playClickSound();
+            setTradeStocksAmount(0);
             setTradeType("buy");
+            setIsAvailableTradeStocks(buyTradeStocks)
             setIsOpenTradeModal(true);
           }}
           className=" w-full border p-1 rounded-md text-white font-bold bg-red-500 hover:bg-red-400"
@@ -62,7 +90,9 @@ export default function TradeButtons() {
         <button
           onClick={() => {
             playClickSound();
+            setTradeStocksAmount(0);
             setTradeType("sell");
+            setIsAvailableTradeStocks(sellTradeStocks)
             setIsOpenTradeModal(true);
           }}
           className="w-full border p-1 rounded-md text-white font-bold bg-blue-500 hover:bg-small-1"
@@ -74,8 +104,10 @@ export default function TradeButtons() {
         <button
           onClick={() => {
             playClickSound();
-            setTradeType('short-selling')
-            setIsOpenTradeModal(true)
+            setTradeStocksAmount(0);
+            setTradeType('short-selling');
+            setIsAvailableTradeStocks(buyTradeStocks)
+            setIsOpenTradeModal(true);
           }}
           className="w-full border p-1 rounded-md text-white font-bold bg-yellow-500 hover:bg-small-10"
         >
@@ -86,10 +118,12 @@ export default function TradeButtons() {
         <button
           onClick={()=>{
             playClickSound();
-            setTradeType('close-short')
-            setIsOpenTradeModal(true)
+            setIsAvailableTradeStocks(closeShort)
+            setTradeStocksAmount(0);
+            setTradeType("close-short");
+            setIsOpenTradeModal(true);
           }}
-         className='w-full border p-1 rounded-md text-white font-bold bg-small-12 hover:bg-small-12'>공매도 청산
+         className='w-full border px-0.5 py-1 rounded-md text-white font-bold bg-small-12 hover:bg-small-12'>공매도청산(C)
         </button>
     </div>
   </div>
