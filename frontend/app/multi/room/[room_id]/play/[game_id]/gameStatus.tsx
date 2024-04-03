94871e0a95369a0bbf129a7ba4df8d7e2f4f1645
dldydtn0805 <ssafysoo95@gmail.com>
2024-04-04 04:08:36 +0900

    feat/#91-multigame-websocket-sse

--- frontend/app/multi/room/[room_id]/play/[game_id]/gameStatus.tsx
+++ frontend/app/multi/room/[room_id]/play/[game_id]/gameStatus.tsx
@@ -17,11 +17,11 @@ export default function GameStatus() {
     unrealizedGain,
   } = socketStore();
   return (
-    <div className="border rounded-md m-1 bg-small-6 text-white row-span-7 grid grid-rows-12 gap-2 items-center">
+    <div className="border rounded-md m-1 bg-small-6 text-white row-span-7 grid grid-rows-6 gap-2 items-center">
       <div className="row-span-1 flex justify-center text-sm">
         <div>초기자산: {initialAsset.toLocaleString()}</div>
       </div>
-      <div className="row-span-3">
+      <div className="row-span-1">
         <div>총 평가 자산</div>
         <div
           className={`${profitMargin > 0 && `text-red-500`} ${
@@ -38,17 +38,27 @@ export default function GameStatus() {
           {unrealizedGain.toLocaleString()}원({profitMargin.toFixed(2)}%)
         </div>
       </div>
-      <div className="row-span-2 grid grid-cols-12 text-sm">
+      <div className="row-span-1 grid grid-cols-12 text-sm">
         <div className="col-span-6">
           <div>보유 현금</div>
           <div>{cash.toLocaleString()}원</div>
         </div>
         <div className="col-span-6">
           <div>주식수</div>
-          <div>{stockAmount}</div>
+          <div>{stockAmount}개</div>
         </div>
       </div>
-      <div className="row-span-2 grid grid-cols-12 text-sm">
+      <div className="row-span-1 grid grid-cols-12 text-sm">
+        <div className="col-span-6">
+          <div>공매도 평균가</div>
+          <div>{shortAveragePrice.toLocaleString()}원</div>
+        </div>
+        <div className="col-span-6">
+          <div>공매도 잔고</div>
+          <div>{shortStockAmount}개</div>
+        </div>
+      </div>
+      <div className="row-span-1 grid grid-cols-12 text-sm">
         <div className="col-span-6">
           <div>주식 매입금</div>
           <div>{totalPurchaseAmount.toLocaleString()}원</div>
@@ -58,7 +68,7 @@ export default function GameStatus() {
           <div>{averagePrice.toLocaleString()}원</div>
         </div>
       </div>
-      <div className="row-span-2 grid grid-cols-12 text-sm">
+      <div className="row-span-1 grid grid-cols-12 text-sm">
         <div className="col-span-6">
           <div>주식 평가금</div>
           <div>{stockValue.toLocaleString()}원</div>
